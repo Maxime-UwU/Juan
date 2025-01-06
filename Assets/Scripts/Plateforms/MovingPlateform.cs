51c62bb8e08d1562618619a1513bc62fb29e91cc
Maxime Maxime <71704827+Maxime-UwU@users.noreply.github.com>
2025-01-06 16:50:57 +0100

    add moving plateform and fix rotation

---
+++ Assets/Scripts/Plateforms/MovingPlateform.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlateform : MonoBehaviour
+{
+    public Transform PointA;
+    public Transform PointB;
+    public float MoveSpeed = 2f;
+
+    private Vector3 nextPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPosition = PointB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, nextPosition, MoveSpeed * Time.deltaTime);
+
+        if(transform.position == nextPosition)
+        {
+            nextPosition = (nextPosition == PointA.position) ? PointB.position : PointA.position;
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.parent = null;
+        }
+    }
+}
